fd5566fa1f9aa194f4ecdfe8d02bfe450a681635
AhmadRendyFirjatullah <a.rendyfirjatullah@gmail.com>
2023-05-15 16:08:05 +0800

    Array

---
+++ src/Array/Array.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        String mataKuliah[] ;
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+
+        System.out.println(mataKuliah[0]);
+        System.out.println(mataKuliah[1]);
+        System.out.println(mataKuliah[2]);
+        System.out.println(mataKuliah[3]);
+        System.out.println(mataKuliah[4]);
+
+
+
+    }
+}
+
